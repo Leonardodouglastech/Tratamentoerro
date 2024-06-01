1c68a03e8f8bfd6e0700ac4a8e3f84bc8ec8bbf2
Leonardodouglastech <167647313+Leonardodouglastech@users.noreply.github.com>
2024-06-01 22:50:35 +0000

    Inicio

---
+++ tratamentoErro.py
@@ -0,0 +1,9 @@
+try:
+  
+    n1 = int(input('Digite o primeiro número: '))
+    n2 = int(input('Digite o segundo número: '))
+    resultado = n1 + n2
+    print(f'A soma de {n1} e {n2} é: {resultado}!')
+       
+except:
+    print('Digite um número válido!')
